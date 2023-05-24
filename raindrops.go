ae9091066991f7366bc2e53c5048aceb9c91fd7e
Emily Grove <eh.grove@yahoo.com>
2023-05-24 16:16:17 -0500

    working solution

--- raindrops.go
+++ raindrops.go
@@ -1,6 +1,28 @@
 package raindrops
+import "strconv"
 
+//If number % 3 == 0, concatenate 'Pling' to result
+//If number % 5 == 0, concatenate 'Plang' to result
+//If number % 7 == 0, concatenate 'Plong' to the result
+//else, return number
 
 func Convert(number int) string {
-	panic("Please implement the Convert function")
+	result := ""
+
+	if number % 3 == 0 {
+		result += "Pling"
+	}
+	if number % 5 == 0 {
+		result += "Plang"
+	}
+	if number % 7 == 0 {
+		result += "Plong"
+	 } 
+	if result == "" {
+		return strconv.Itoa(number)
+	}
+
+	return result
+	
+	
 }
